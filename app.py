eef03bfa835e3d4a3b460d36f34af21ae053dff7
Amin Najafgholizadeh <a.gholizadeh@ariansine.local>
2024-11-22 19:52:34 +0330

    added some new features

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
